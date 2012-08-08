d6eff4a516b4adef0e53ded2fe963c905d88baa2
glefur <glefur>
2012-08-08 13:38:37 +0000

    UPDATED: trying to come back to a stable version

--- index.php
+++ index.php
@@ -13,8 +13,7 @@
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
  	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
- 	//require_once($_SERVER['DOCUMENT_ROOT'] . "./functions/templating.php");
- 	$App 	= new App();
+	$App 	= new App();
 	$Nav	= new Nav();
 	$Menu 	= new Menu();
 
@@ -22,7 +21,7 @@
 	$localVersion = false;
 	
 	// 	# Paste your HTML content between the EOHTML markers!
-	$html = get_file_contents('pages/template.html');
+	$html = file_get_contents('pages/template.html');
 
 	# Generate the web page
 	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
