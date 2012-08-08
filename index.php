be6763fec3b564b3191a46d48168b9d7b907c481
glefur <glefur>
2012-08-08 12:53:43 +0000

    UPDATED: architecture

--- index.php
+++ index.php
@@ -21,11 +21,7 @@
 	$localVersion = false;
 	
 	// 	# Paste your HTML content between the EOHTML markers!
-	if ($_GET['section'] == "support") {
-		$html = file_get_contents('pages/_support.html');
-	} else {
-		$html = file_get_contents('pages/_index.html');
-	}
+	$html = file_get_contents('pages/template.html');
 
 	# Generate the web page
 	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
